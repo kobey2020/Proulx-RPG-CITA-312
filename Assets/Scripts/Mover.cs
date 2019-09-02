f3a07e23b1ff3dfcbac068a69cc8afe288da9ecb
kobey2020 <kobey2020@yahoo.com>
2019-09-02 11:41:35 -0400

    Episode 16 Complete

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -14,6 +14,7 @@ public class Mover : MonoBehaviour
         {
             MoveToCursor();
         }
+        UpdateAnimator();
     }
 
     private void MoveToCursor()
@@ -26,6 +27,11 @@ public class Mover : MonoBehaviour
             GetComponent<NavMeshAgent>().destination = hit.point;
         }
     }
-
-
+    private void UpdateAnimator()
+    {
+        Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+        float speed = localVelocity.z;
+        GetComponent<Animator>().SetFloat("forwardSpeed", speed);
+    }
 }
