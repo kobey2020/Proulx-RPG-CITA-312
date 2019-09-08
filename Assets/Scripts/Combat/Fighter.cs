63a42bf5541135b49813e9c129170e5f889d9c7e
kobey2020 <kobey2020@yahoo.com>
2019-09-08 13:58:08 -0400

    Episode 26 Completed

---
+++ Assets/Scripts/Combat/Fighter.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace RPG.Combat
+{
+    public class Fighter : MonoBehaviour
+    {
+
+        public void Attack(CombatTarget target)
+        {
+            print("Take that you short, squat peasant!");
+        }
+
+    }
+}
