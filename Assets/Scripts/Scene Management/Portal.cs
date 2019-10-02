aaab1040f3d4c658622759cafa740c67e054f668
kobey2020 <kobey2020@yahoo.com>
2019-10-02 19:13:28 -0400

    Lecture 72 complete, Scene 2 portal doesnt work

--- Assets/Scripts/Scene Management/Portal.cs
+++ Assets/Scripts/Scene Management/Portal.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -8,13 +9,43 @@ namespace RPG.SceneManagement
     public class Portal : MonoBehaviour
     {
         [SerializeField] int sceneTotLoad = -1;
+        [SerializeField] Transform spawnPoint;
 
         private void OnTriggerEnter(Collider other)
         {
             if (other.tag == "Player")
             {
-                SceneManager.LoadScene(sceneTotLoad);
+                StartCoroutine(Transition());
             }
+        }
+        private IEnumerator Transition()
+        {
+            DontDestroyOnLoad(gameObject);
+            yield return SceneManager.LoadSceneAsync(sceneTotLoad);
+
+            Portal otherPortal = GetOtherPortal();
+            UpdatePlayer(otherPortal);
+
+            Destroy(gameObject);
+        }
+
+        private void UpdatePlayer(Portal otherPortal)
+        {
+            GameObject player = GameObject.FindWithTag("Player");
+            player.transform.position = otherPortal.spawnPoint.position;
+            player.transform.rotation = otherPortal.spawnPoint.rotation;
+        }
+
+        private Portal GetOtherPortal()
+        {
+            foreach (Portal portal in FindObjectsOfType<Portal>())
+            {
+                if (portal == this) continue;
+
+                return portal;
+            }
+
+            return null;
         }
     }
 }
